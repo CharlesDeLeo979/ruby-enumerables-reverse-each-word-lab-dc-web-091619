f782b0b9897277891bd350a19246f8166bc21d0d
Charles DeLeo <charles.deleo979@gmail.com>
2019-09-08 17:01:18 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,5 @@
-<<<<<<< HEAD
 def reverse_each_word(string)
   string.split.collect { |word| word.reverse! }.join(" ")
   
 end
-=======
-def reverse_each_word("Hello there, and how are you?")
->>>>>>> 84a9c69f376d2e69b306a4265cb45f2a90c06401
+
